0be7580a7a91ea234db2b56888df170a36374ea9
Himanshu Lilhare <rajlilhare200@gmail.com>
2023-09-02 15:39:02 +0530

    insert data with callback

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import express from "express"
 import redis from "redis"
+import util from "util"
 const redis_url = 'redis://localhost:6379'
 const client = redis.createClient(redis_url)
 
 const app = express()
+app.use(express.json())
+
+app.post('/',(req,res)=>{
+    const {key,value} = req.body
+console.log(key,value)
+ client.set(key,value,(err,response)=>{
+
+    console.log(response)
+    res.send(response)
+ })
+
+ })
+
 
 app.listen(3000,()=>{
     console.log("listen to the port xyxyxyx")
