10037eb4718ab857170d609c7a19427f46505ef7
Himanshu Lilhare <rajlilhare200@gmail.com>
2023-09-02 15:41:31 +0530

    insert-data-with-promise

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-import express from "express"
-import redis from "redis"
-import util from "util"
-const redis_url = 'redis://localhost:6379'
-const client = redis.createClient(redis_url)
+import express from "express";
+import redis from "redis";
+import util from "util";
+const redis_url = "redis://localhost:6379";
+const client = redis.createClient(redis_url);
+client.set = util.promisify(client.set);
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+app.post("/", async (req, res) => {
+  const { key, value } = req.body;
+  console.log(key, value);
+  const response = await client.set(key, value);
 
-app.post('/',(req,res)=>{
-    const {key,value} = req.body
-console.log(key,value)
- client.set(key,value,(err,response)=>{
+  res.json(response);
+});
 
-    console.log(response)
-    res.send(response)
- })
-
- })
-
-
-app.listen(3000,()=>{
-    console.log("listen to the port xyxyxyx")
-})
+app.listen(3000, () => {
+  console.log("listen to the port xyxyxyx");
+});
